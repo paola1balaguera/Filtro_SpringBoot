7873256cbee06012a7a0aebca0d62e66b79ffa63
paola1balaguera <140655187+paola1balaguera@users.noreply.github.com>
2024-04-11 19:48:45 -0500

    Delete Exeptions directory

--- Exeptions/NombreInvalidoException.java
+++
@@ -1,7 +0,0 @@
-package agencia.inmobiliaria.Exeptions;
-
-public class NombreInvalidoException extends RuntimeException {
-    public NombreInvalidoException(String message) {
-        super(message);
-    }
-}
--- Exeptions/NotFoundElementsException.java
+++
@@ -1,7 +0,0 @@
-package agencia.inmobiliaria.Exeptions;
-
-public class NotFoundElementsException extends RuntimeException {
-    public NotFoundElementsException(String message) {
-        super(message);
-    }
-}
